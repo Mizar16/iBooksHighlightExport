62c8e6cdd4933eb1569395270bd3823732a940bc
damicoac <damicoac@gmail.com>
2012-09-15 11:39:15 -0400

    updated script to output in markdown. The format could still use some work but it is ok for now. Also fixed a bug to remove any / from a book name. This avoids file system errors.

--- iBookExport2.rb
+++ iBookExport2.rb
@@ -46,29 +46,32 @@ bookKeyTileDB.execute("select ZDATABASEKEY, ZBOOKTITLE from ZBKBOOKINFO") do |ro
 
    	bookNotesDB.execute("select ZANNOTATIONASSETID, ZANNOTATIONTYPE from ZAEANNOTATION where ZANNOTATIONTYPE=2 and ZANNOTATIONASSETID = ?", bookNameKey) do |noteExistTest|
 		if noteExistTest[0] != nil
-			bookFile = File.open("bookNote/"+bookName[0,30]+".txt","w+")
+			#remove and / from a book name
+			*bookName = bookName.split("/")
+			#convert the array to a string
+			bookName = bookName.join(" ")
+			
+			#open the file
+			bookFile = File.open("bookNote/"+bookName[0,29]+".md","w+")
 			#print book name
-			bookFile << bookName+"\n"
-			bookFile << "---------------------------------\n\n"
+			bookFile << "##"+bookName+"\n"
 			bookFile.close()
 		
 			#at each point do a query for notes or highlights
 			bookNotesDB.execute("select ZANNOTATIONNOTE, ZANNOTATIONSELECTEDTEXT, ZANNOTATIONASSETID, ZPLLOCATIONRANGESTART, ZANNOTATIONTYPE, ZANNOTATIONDELETED from ZAEANNOTATION where ZANNOTATIONTYPE=2 and ZANNOTATIONDELETED = 0 and ZANNOTATIONASSETID = ? order by ZPLLOCATIONRANGESTART", bookNameKey) do |notes|
 		
-				bookFile = File.open("bookNote/"+bookName[0,30]+".txt","a")
+				bookFile = File.open("bookNote/"+bookName[0,29]+".md","a")
 
 				#add those to the book class obj
 				if notes[1]!=nil
 					#	puts "::"+ notes[0] + "::"+notes[1]+"::"+notes[2]
 					#	notes[0] is note, notes[1] is highlight
 					#	print notes and highlights
-					bookFile << "Highlight:\n"
-					bookFile << "---------------------------------\n"
-					bookFile << "		"+notes[1]+"\n\n"
+					bookFile << "###Highlight:\n\n"
+					bookFile << notes[1]+"\n\n"
 					if notes[0]!=nil
-						bookFile << "Note:\n"
-						bookFile << "---------------------------------\n"
-						bookFile << "		"+notes[0]+"\n\n\n"
+						bookFile << "#####Note:\n"
+						bookFile << notes[0]+"\n\n\n"
 					end
 				end
 				bookFile.close()
